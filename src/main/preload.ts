ad1ff045c9fdee50533bcae605bfe194fe049e59
takumura <takumura@outlook.com>
2021-05-10 00:25:10 +0900

    refactor: remove unnecessary comments

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -4,11 +4,5 @@ const { contextBridge, ipcRenderer } = require('electron')
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('api', {
   getInitialState: () => ipcRenderer.invoke('GET_STORE_VALUE', 'data'),
-  // {
-  //   ipcRenderer
-  //     .invoke('GET_STORE_VALUE')
-  //     .then((result: any) => result)
-  //     .catch((err: string) => console.log(err))
-  // },
   syncStore: (state: string) => ipcRenderer.send('SYNC_STORE', state),
 })
